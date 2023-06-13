35b312d63572c411f3d1d6f782fabab784550c49
Iulian Topal <itopal@tacitknowledge.com>
2023-06-13 16:32:21 +0800

    -page layout and title fixed

--- src/pages/quickorder/QuickOrderPage.js
+++ src/pages/quickorder/QuickOrderPage.js
@@ -4,7 +4,7 @@ import QuickOrderSelection from './QuickOrderSelection'
 import './quickorder.css'
 const QuickOrderPage = () => {
   return (
-    <div className="quickorder-page-wrapper">
+    <div className="quickorder-page-wrapper ml-[96px]">
       <div className="md:pt-24 pt-[58px] w-full gap-12 md:flex">
         <QuickOrderContent />
         <QuickOrderSelection />
