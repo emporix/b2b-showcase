729eee7ecb2f4869d3caba68d3374005377f3697
Adrian Fella <adrian.fella@neteleven.de>
2023-08-04 11:52:56 +0200

    homepage layout adjustments

--- src/pages/home/About.js
+++ src/pages/home/About.js
@@ -19,7 +19,9 @@ const About = () => {
         mainImageRight.fields.file &&
         mainImageRight.fields.file.url
       ) {
-        setIntroImageUrl(mainImageRight.fields.file.url)
+        const dummyImage = "../../assets/homepage-banner.png";
+        //setIntroImageUrl(mainImageRight.fields.file.url)
+        setIntroImageUrl(dummyImage);
       }
     })()
   }, [mainImageRight])
@@ -29,19 +31,23 @@ const About = () => {
       // style={{ backgroundImage: `url(${landingBg})` }}
       className="home_about"
     >
-    <div className="mx-6 md:ml-16 mt-[48px] md:mt-[114px] w-[492px]">
-          <div className="text-[48px] md:text-[48px] font-inter font-semibold md:leading-[64px] leading-[56px]">
+    <div className="mx-6 md:ml-16 mt-[48px] md:mt-[114px] w-[492px] text-eerieBlack">
+        <div className="text-[48px] md:text-[48px] font-inter font-semibold md:leading-[64px] leading-[56px]">
             {fields.mainTitle}
-          </div>
-          <div className="text-[18px] leading-[30px] font-inter font-normal pt-[24px] md:max-w-[525px]">
+        </div>
+        <div className="text-[18px] leading-[30px] font-inter font-normal pt-[24px] md:max-w-[525px]">
             {fields.companyMission}
-          </div>
-          <div className="pt-[44px] desktop_only text-sm">
-              <button className="px-6 py-4 font-semibold bg-yellow text-eerieBlack rounded">
+        </div>
+        <div className="pt-[44px] desktop_only text-sm">
+              <button className="px-6 py-4 font-semibold bg-primary text-white rounded">
                 {fields.startShoppingButtonLabel}
               </button>
           </div>
-        </div>      
+        </div>
+
+        <div className="">
+          <img src="/img/homepage-banner.png" />
+        </div>
     </div>
   )
 }
