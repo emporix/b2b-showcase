ca9d9dc7ef23a3f20e414317cb7251580ea82505
annanet11 <anna-lena.ehlers@neteleven.de>
2023-09-15 11:38:52 +0200

    added export default for config

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,7 @@
-export const config = {
+const config = {
   authCookieName: process.env.REACT_APP_AUTH_COOKIE_NAME || 'X-Authenticaiton-Token',
   commerceURL: process.env.REACT_APP_GQL_API_URL || '',
   customerCookie: process.env.REACT_APP_CUSTOMER_COOKIE_NAME || ''
 }
+
+export default config;
