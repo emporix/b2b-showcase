9a7ea7de2ceb2417587667b32e9b93a8cdd51389
JochamJulian <julian.jocham@neteleven.de>
2023-10-24 16:55:56 +0200

    dealer page styling

--- src/pages/storefinder/storefinder-table/index.js
+++ src/pages/storefinder/storefinder-table/index.js
@@ -3,58 +3,60 @@ import {getDealers} from "../../../services/storefinder.service";
 import {useEffect, useState} from "react";
 
 const StoreFinderTable = () => {
-	const [dealers, setDealers] = useState([])
-	const getData = async () => {
-		const dealerData = await getDealers()
-		setDealers(dealerData.data.dealers)
-	};
-	useEffect(() => {
-		getData();
-	}, [])
+    const [dealers, setDealers] = useState([])
+    const getData = async () => {
+        const dealerData = await getDealers()
+        setDealers(dealerData.data.dealers)
+    };
+    useEffect(() => {
+        getData();
+    }, [])
 
 
-	return (
-		<div className='storefinderTable'>
-			Storefinder Table
+    return (
+        <div className='storefinderTable'>
+            <div className='storefinderTable__table__header'>
+                Unsere Lokalen Verkaufspartner
+            </div>
+            <div className='storefinderTable__table'>
+                <div className='storefinderTable__table__items'>
+                    {dealers.map((store, index) => {
+                        if (store.name) {
+                            return (
+                                <div className='storefinderTable__table__item' key={store.id}>
+                                    <img src='/img/wineDealerBackground.png'
+                                         alt="Store Image"
+                                         style={{borderRadius: '5px'}}/>
+                                    <div className='storefinderTable__table__item__key'>
+                                        <strong style={{fontSize: '1.5rem'}}>{store.name}</strong>
+                                    </div>
 
-			<div className='storefinderTable__table'>
-				<div className='storefinderTable__table__header'>
 
-				</div>
-				<div className='storefinderTable__table__items'>
-					{dealers.map((store, index) => {
-						if (store.name) {
-							return (
-								<div className='storefinderTable__table__item' key={store.id}>
-									<div className='storefinderTable__table__item__key'>
-										<strong>{store.name}</strong>
-									</div>
-									<div className='storefinderTable__table__item__key'>
-										<a href={'mailto:' + store.email} style={{color: 'blue'}}>{store.email}</a>
-									</div>
+                                    <div className='storefinderTable__table__item__key'>
+                                        {store.street ? store.street + " " + (store.houseNumber ? store.houseNumber : "") : ""}
+                                    </div>
+                                    <div className='storefinderTable__table__item__key'>
+                                        {store.zipCode ? store.zipCode + " " + (store.city ? store.city : "") : store.city}
+                                    </div>
 
-									<div className='storefinderTable__table__item__key'>
-										{store.street ? store.street + " " + (store.houseNumber ? store.houseNumber : "") : ""}
-									</div>
-									<div className='storefinderTable__table__item__key'>
-										{store.zipCode ? store.zipCode + " " + (store.city ? store.city : "") : store.city}
-									</div>
-
-									<div className='storefinderTable__table__item__key'>
-										{store.phoneNumber}
-									</div>
-									<div className='storefinderTable__table__item__key'>
-										<a href={store.website} style={{color: 'blue'}}>
-											{store.websiten}
-										</a>
-									</div>
-								</div>)
-						}
-					})
-					}
-				</div>
-			</div>
-		</div>
-	)
+                                    <div className='storefinderTable__table__item__key'>
+                                        {store.phoneNumber}
+                                    </div>
+                                    <div className='storefinderTable__table__item__key'>
+                                        <a href={store.website}
+                                           style={{color: 'blue'}}>{store.website}
+                                        </a>
+                                    </div>
+                                    <div className='storefinderTable__table__item__key'>
+                                        <a href={'mailto:' + store.email} style={{color: 'blue'}}>{store.email}</a>
+                                    </div>
+                                </div>)
+                        }
+                    })
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
 export default StoreFinderTable
