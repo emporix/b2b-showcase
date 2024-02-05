f04dbd8c6ed4a9c0cf385a6bace555c407276d92
annanet11 <anna-lena.ehlers@neteleven.de>
2024-02-05 13:12:07 +0100

    adjustments according to comments; changed hierarchy in template and rearranged copyright footer

--- src/components/Cms/footer.js
+++ src/components/Cms/footer.js
@@ -12,8 +12,8 @@ const socialMediaComponentMap = {
 const CMS_DESKTOP_FOOTER = (props) => {
 
   return (
-      <div className="bg-aliceBlue">
-          <div className="desktop_only_flex  md:py-12 max-w-screen-xl mx-auto ">
+      <div className="desktop_only bg-aliceBlue">
+          <div className="flex md:py-12 max-w-screen-xl mx-auto ">
             { props.content.footer.map((column) => (
               <div className="mx-auto">
                   <ul className="text-base">
@@ -21,7 +21,7 @@ const CMS_DESKTOP_FOOTER = (props) => {
                       {column.items.map((item) => (
                         item?.data?.lt_text ? 
                           (
-                            <Link to={item?.data?.lt_url} ><li className=" font-light pt-4">{item?.data?.lt_text}</li></Link>
+                            <li className=" font-light pt-4"><Link to={item?.data?.lt_url} >{item?.data?.lt_text}</Link></li>
                           ) :
                           (
                             <li className="font-light pt-5">{item?.content[0]?.content}</li>
@@ -31,27 +31,7 @@ const CMS_DESKTOP_FOOTER = (props) => {
               </div>
             ))}
           </div>
-          <div className="p-4 bg-primary text-manatee flex justify-between align-center">
-              <div>
-                  <p className="text-bgWhite font-bold ">
-                    {props.content.copyright.copy}
-                  </p>
-              </div>
-              <div className="flex">
-                  <div className="flex pl-6 pt-[-2px] text-bgWhite">
-                    { props.content.copyright.items.map((item) => {
-                      const IconComponent = socialMediaComponentMap[item.data.lt_text]
-                    return ( 
-                      <a href={item.data.lt_url} >
-                        <div className="pl-6 pt-[-2px] text-bgWhite flex items-center">
-                          <IconComponent size={30}/>
-                        </div>
-                      </a>
-                      )
-                    })}
-                  </div>
-              </div>
-          </div>
+          
 
       </div>
   )
@@ -59,24 +39,46 @@ const CMS_DESKTOP_FOOTER = (props) => {
 
 const CMS_MOBILE_FOOTER = (props) => {
   return (
-    <div className='flex w-full flex-wrap gap-y-4 pt-4 pb-8 md:py-0 bg-aliceBlue'>
+    <div className='mobile_only flex w-full flex-wrap gap-y-4 pt-4 pb-8 md:py-0 bg-aliceBlue'>
       { props.content.footer.map((column) => (
-        <ul className="mobile_only text-primary font-bold text-base px-6 text-center w-1/2">
-          <div className=''>
+        <ul className="text-primary font-bold text-base px-6 text-center w-1/2">
             <li className="py-4">{column.headline}</li>
             {column.items.map((item) => (
               item?.data?.lt_text ? 
               (
-                <Link to={item?.data?.lt_url} ><li className=" font-light pt-2">{item?.data?.lt_text}</li></Link>
+                <li className=" font-light pt-2"><Link to={item?.data?.lt_url} >{item?.data?.lt_text}</Link></li>
               ) :
               (
                 <li className="font-light pt-2">{item?.content[0]?.content}</li>
               )
             ))}
-          </div>
         </ul>
       ))}
     </div>
+  )
+}
+
+const COPYRIGHT_FOOTER = (props) => {
+  return (
+    <div className="p-4 bg-primary text-manatee flex justify-between align-center">
+      <p className="text-bgWhite font-bold ">
+        {props.content.copyright.copy}
+      </p>
+      <div className="flex">
+          <div className="flex pl-6 pt-[-2px] text-bgWhite">
+            { props.content.copyright.items.map((item) => {
+              const IconComponent = socialMediaComponentMap[item.data.lt_text]
+            return ( 
+              <a href={item.data.lt_url} >
+                <div className="pl-6 pt-[-2px] text-bgWhite flex items-center">
+                  <IconComponent size={30}/>
+                </div>
+              </a>
+              )
+            })}
+          </div>
+      </div>
+  </div>
   )
 }
 
@@ -85,6 +87,7 @@ const CMS_Footer = (props) => {
       <div className="footer">
           <CMS_MOBILE_FOOTER content={props.props}/>
           <CMS_DESKTOP_FOOTER content={props.props}/>
+          <COPYRIGHT_FOOTER content={props.props}/>
       </div>
     )
 }
