7e503e21172b7355a95eac6a3497e5abbe918705
Annika <annika.ryll@neteleven.de>
2024-04-13 12:45:47 +0200

    breadcrump home button kein 404 mehr.

--- src/pages/product/ProductDetailPage.js
+++ src/pages/product/ProductDetailPage.js
@@ -20,7 +20,7 @@ import SliderComponent from '../../components/Utilities/slider'
 import Accordion, { AccordionItem } from '../../components/Utilities/accordion'
 
 import LayoutContext from '../context'
-import { productUrl } from '../../services/service.config'
+import { homeUrl, productUrl } from '../../services/service.config'
 
 import { LargePrimaryButton } from '../../components/Utilities/button'
 import {
@@ -56,7 +56,7 @@ const Bold = ({ children }) => {
   return <div className="font-bold">{children}</div>
 }
 const ProductDetailCategoryCaptionBar = ({ category }) => {
-  const categoryTree = [{ caption: 'Home', link: productUrl() }]
+  const categoryTree = [{ caption: 'Home', link: homeUrl() }]
   let lnk = productUrl()
   for (let c in category) {
     lnk = `${lnk}/${category[c].toLowerCase().replaceAll(' ', '_')}`
