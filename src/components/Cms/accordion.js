0f6f4e04cc6e4dcd7e5b02c44254bdfe9f3b2e36
yoshi <yoshihiro.kanai@neteleven.de>
2024-06-04 17:35:50 +0200

    bugfix margins

--- src/components/Cms/accordion.js
+++ src/components/Cms/accordion.js
@@ -5,11 +5,11 @@ import Accordion, { AccordionItem } from '../Utilities/accordion'
 // for very simple lists only (glossary maybe)
 
 export const CMS_Accordion = (props) => {
-  console.log(props.props)
+  const { displayName, data } = props.props
   return (
     <Accordion>
-      <AccordionItem index={0} title={props.props.displayName}>
-        <FsGenericComponentList componentData={props.props.data.st_elements} />
+      <AccordionItem index={0} title={displayName}>
+        <FsGenericComponentList componentData={data.st_elements} />
       </AccordionItem>
     </Accordion>
   )
--- src/components/Cms/buttonLink.js
+++ src/components/Cms/buttonLink.js
@@ -1,6 +1,6 @@
 export const ButtonLink = (props) => {
   return (
-    <button className="cta-primary block mx-auto md:mx-0">{props.props.data.st_buttonLink.data.lt_text}</button>
+    <button className="cta-primary block mx-auto md:mx-24">{props.props.data.st_buttonLink.data.lt_text}</button>
     //todo: link to some/where?
   )
 }
