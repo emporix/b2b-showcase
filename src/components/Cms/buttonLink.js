645606ffda88f48f7bb89bf34b1b7a0dd21e86e8
yoshi <yoshihiro.kanai@neteleven.de>
2024-06-17 10:35:57 +0200

    adjust styling for buttons

--- src/components/Cms/buttonLink.js
+++ src/components/Cms/buttonLink.js
@@ -1,5 +1,4 @@
 export const ButtonLink = ({ props }) => {
-  console.log(props)
   return (
     <div className="mx-4 mx-adjust">
       <button className="cta-primary block mx-auto md:mx-0">{props.data.st_buttonLink.data.lt_text}</button>
