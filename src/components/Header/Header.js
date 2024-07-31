71d7a999e7c6a9b0c5d87bdd3428443a53515a8e
roettingerj <joachim.roettinger@ecube.de>
2024-07-31 16:41:34 +0200

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,68 +1,13 @@
-import React, { useState } from 'react'
-import Navbar from './navigationbar'
+import React from 'react'
+import Navbar from './Navbar'
 import './topbar.css'
-import AlgoliaSearchbar from '../AlgoliaSearchbar'
-import { Logo } from '../Logo'
-import { APPLICATION_ID } from '../../constants/localstorage'
-import MegaNav from './MegaNav'
-
-const TopNav = ({ blok }) => {
-  const [showMegaMenuContent, setShowMegaMenuContent] = useState(false)
-
-  return (
-    <div className="desktop_only_flex w-full md:h-[136px] fixed z-10 bg-aldiBlue1">
-      <div
-        className="px-10 pt-[76px] w-full  flex xl:px-24  h-[136px] border border-aldiBlue1">
-        <div
-          className="menu-wrapper flex w-full"
-          onMouseLeave={() => {
-            setShowMegaMenuContent(false)
-          }}
-        >
-          <div className="flex w-full h-10">
-            <div
-              className="flex flex-row justify-between pr-[8px] w-[470px] ml-[-97px] pl-[140px] bg-aldiBlue4 h-[81px] mt-[-22px]">
-              <div className="mt-[12px]">
-                <Logo onMouseOver={() => setShowMegaMenuContent(false)} />
-              </div>
-              <span
-                className="hidden xl:block text-white font-aldiCondensed tracking-[2px] mt-[8px] text-[42px]">ALDI</span>
-            </div>
-            <div className="w-full flex flex-row justify-between">
-              <span
-                className="hidden lg:block ml-[8px] mt-[-13px] text-white font-bold font-aldiCondensed tracking-[2px] text-[42px]">ONLINESHOP</span>
-
-              <div className="ml-14 mt-[-9px]">
-                <MegaNav
-                  blok={blok}
-                  showMegaMenuContent={showMegaMenuContent}
-                  setShowMegaMenuContent={setShowMegaMenuContent}
-                />
-              </div>
-
-              <div
-                className="hidden lg:flex ml-20"
-                onMouseOver={() => setShowMegaMenuContent(false)}
-              >
-                <>
-                  {localStorage.getItem(APPLICATION_ID) &&
-                    (<AlgoliaSearchbar />)
-                  }
-                </>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+import Menu from './Menu'
 
 const Header = ({ blok }) => {
   return (
     <>
       <Navbar blok={blok}/>
-      <TopNav blok={blok} />
+      <Menu blok={blok} />
     </>
   )
 }
