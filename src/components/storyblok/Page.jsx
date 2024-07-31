08d4e62c83234f67d4c3a1a756d9eaf6d857e98d
roettingerj <joachim.roettinger@ecube.de>
2024-07-31 11:30:41 +0200

    page start

--- src/components/storyblok/Page.jsx
+++ src/components/storyblok/Page.jsx
@@ -2,11 +2,13 @@ import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 
 const Page = ({ blok }) => (
   <main {...storyblokEditable(blok)}>
-    {blok.body
-      ? blok.body.map((blok) => (
-        <StoryblokComponent blok={blok} key={blok._uid} />
-      ))
-      : null}
+    {blok.body && blok.body.map((blok, index) => {
+      return index !== 1 ? <StoryblokComponent blok={blok} key={blok._uid} /> :
+        <>
+          <div className="h-[80px] md:h-[136px]" />
+          <StoryblokComponent blok={blok} key={blok._uid} />
+        </>
+    })}
   </main>
 );
 
