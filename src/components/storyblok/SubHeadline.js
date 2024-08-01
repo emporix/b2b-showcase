d72fee85918514dad705a58c9d234c793c1161dd
roettingerj <joachim.roettinger@ecube.de>
2024-08-01 13:48:28 +0200

    component fix

--- src/components/storyblok/SubHeadline.js
+++ src/components/storyblok/SubHeadline.js
@@ -1,6 +1,9 @@
+import { storyblokEditable } from '@storyblok/react'
+
 const SubHeadline = ({ blok }) => {
   return (<h3
-    className="mx-4 xl:mx-auto w-full max-w-screen-xl my-10 text-aldiBlue4 text-3xl font-bold">{blok.text}</h3>)
+    className="mx-4 xl:mx-auto w-full max-w-screen-xl my-10 text-aldiBlue4 text-3xl font-bold" {...storyblokEditable(
+    blok)}>{blok.text}</h3>)
 }
 
 export default SubHeadline
