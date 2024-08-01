3d236f45e5f6ec24f4611c0f4f7f7199ffa1a391
JochamJulian <julian.jocham@neteleven.de>
2024-08-01 17:13:02 +0200

    fix previewId for Accordion, previewId for headline, and link

--- src/components/Cms/accordion.js
+++ src/components/Cms/accordion.js
@@ -7,10 +7,12 @@ import Accordion, {AccordionItem} from '../Utilities/accordion'
 export const CMS_Accordion = (props) => {
   const { displayName, data } = props.props
   return (
-    <Accordion data-preview-id={props?.props?.previewId}>
-      <AccordionItem index={0} title={displayName}>
-        <FsGenericComponentList componentData={data.st_elements} />
-      </AccordionItem>
-    </Accordion>
+      <div data-preview-id={props?.props?.previewId}>
+          <Accordion>
+              <AccordionItem index={0} title={displayName}>
+                  <FsGenericComponentList componentData={data.st_elements} />
+              </AccordionItem>
+          </Accordion>
+      </div>
   )
 }
--- src/components/Cms/buttonLink.js
+++ src/components/Cms/buttonLink.js
@@ -1,6 +1,6 @@
 export const ButtonLink = ({ props }) => {
   return (
-    <div className="mx-4 mx-adjust">
+    <div data-preview-id={props?.previewId} className="mx-4 mx-adjust">
       <button className="cta-primary block mx-auto md:mx-0">{props.data.st_buttonLink.data.lt_text}</button>
     </div>
     //todo: link to some/where?
